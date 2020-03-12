e974dba239e0aea9cd920ef93fd50958139a49b4
adnanz <zderoadnan@gmail.com>
2020-03-12 15:41:23 +0100

    Tutorijal 1 - HelloWorld

---
+++ src/sample/Controller.java
@@ -0,0 +1,5 @@
+package sample;
+
+public class Controller {
+    //Iz nekog razloga kada dodam OnAction atribut button-u, ne mogu dodati metodu koja bi izvrsila neku naredbu jer dobijam errore koje nisam mogao sam popraviti.
+}
